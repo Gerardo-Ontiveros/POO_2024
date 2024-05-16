1b115cb1ea9e9ac4d76770fb7617a5fcc0c62a79
Reexxy <gontiverosdavila01@gmail.com>
2024-05-16 10:44:01 -0600

    Upload Activities

---
+++ parcial1_2A/5_Estructuras_Control_Ciclicas/for.py
@@ -0,0 +1,15 @@
+'''
+    Ciclo For estructura interativa que se ejecuta X veces
+
+    Sintaxis:
+        for variable in range(inicio, fin, incremento):
+            bloque de codigo
+
+
+    Ejemplo 1 Crear que imprima en pantalla 5 veces el @
+'''
+
+
+
+for i in range(5):
+    print('@')
