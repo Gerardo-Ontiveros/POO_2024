bb3671ba9db41156f572dc73bad864ad899b83d1
Reexxy <gontiverosdavila01@gmail.com>
2024-05-20 08:32:55 -0600

    Upload review exercises

--- parcial1_2A/6_Ejercicio_Repaso/Ejercicio_1.py
+++ parcial1_2A/6_Ejercicio_Repaso/Ejercicio_1.py
@@ -1,15 +1,13 @@
 """
- Crear un programa que permita calcular e imprimir el precio a pagar por un articulo.
- en el precio a pagar se incluye el IVA el programa debera de funcionar n veces como el usuario desee.
+1.- Crear 2 variables una pais y otra continente 
+Mostrar su valor en pantalla
+Poner un comentario para poner el tipo de dato
+
 """
 
-new_product = "s"
-while new_product == "s":
-    price = float(input("Ingresa el precio del producto: "))
-    IVA = price * .16
-    price_total = price + IVA
-    print(f"Subtotal: ${price}")
-    print(f"IVA: ${IVA}")
-    print(f"El precio total del producto es de: ${price_total}")
-    print(f"deseas agregar otro producto? (S/N)")
-    new_product = input("")
+pais = input("Ingrese el nombre del país: ")
+continente = input("Ingrese el nombre del continente: ")
+
+print(f"El país es: {pais} y el continente es: {continente}")
+print(f"El tipo de dato de la variable pais es: {type(pais)}")
+
