492eab40545caab8266f93f95940796572fce549
Reexxy <gontiverosdavila01@gmail.com>
2024-05-23 09:19:51 -0600

    Update Ejercicio_10

--- parcial1_2A/6_Ejercicio_Repaso (1-10)/Ejercicio_10.py
+++ parcial1_2A/6_Ejercicio_Repaso (1-10)/Ejercicio_10.py
@@ -4,11 +4,12 @@ la calificacion de 15 alumnos e imprimir en pantalla cuantos aproboron y cuantos
 """
 j=0
 k=0
-for i in range(1, 15):
-    n = int(input(f"Ingresa la calificacion {i}: "))
+for i in range(15):
+    n = int(input(f"Ingresa la calificacion: "))
     if n < 80:
         j += 1
-        print(f"El numero de reprobados fue de {j}")
+        
     elif n > 80:
-        k += 1     
-        print(f"El numero de aprobados fue de {k}")
+        k += 1
+print(f"El numero de reprobados fue de {j}")     
+print(f"El numero de aprobados fue de {k}")
