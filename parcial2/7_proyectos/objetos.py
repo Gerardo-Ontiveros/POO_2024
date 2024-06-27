336e1e27c29405ee03438ab9402779c9e2862710
Reexxy <gontiverosdavila01@gmail.com>
2024-06-27 09:59:15 -0600

    ER Parcial 2 Figuras Geometricas

---
+++ parcial2/7_proyectos/objetos.py
@@ -0,0 +1,10 @@
+from clases import *
+
+circulo=Circulo(40,3.1416)
+print(f"Area circulo: {circulo.CalcularArea()}")
+
+rectangulo=Rectangulo(10,5)
+print(f"Area rectangulo: {rectangulo.CalcularArea()}")
+
+triangulo=Triangulo(15,10)
+print(f"Area triangulo: {triangulo.CalcularArea()}")
